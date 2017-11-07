b2bfc9ea902cec5f4097b8f75c4fb14206b33197
githubca <hanson.chen@gmail.com>
2017-11-06 23:58:25 -0500

    add featuers

--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
